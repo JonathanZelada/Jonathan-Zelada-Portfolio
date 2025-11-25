599bb345b7a94cefab1082528e9046fb2b4b4393
jona <153209214+JonathanZelada@users.noreply.github.com>
2025-11-25 10:34:21 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,21 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools';
 
-// https://vite.dev/config/
-export default defineConfig({
-  base: '/Jonathan-Zelada-Portfolio/',
-  plugins: [
-    tailwindcss(),
-    vue(),
-    vueDevTools(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+export default defineConfig(({ command }) => {
+  return {
+    base: '/Jonathan-Zelada-Portfolio/',
+    plugins: [
+      tailwindcss(),
+      vue(),
+      // Only load vue devtools in dev mode
+      command === 'serve' ? require('vite-plugin-vue-devtools').default() : null,
+    ].filter(Boolean),
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      },
     },
-  },
+  }
 })
