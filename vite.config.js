a83d5f0f63d780aa1f8ac4df555fed2059cc2173
jona <153209214+JonathanZelada@users.noreply.github.com>
2025-11-25 09:46:47 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ import vueDevTools from 'vite-plugin-vue-devtools';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: 'Jonathan-Zelada-Portfolio',
   plugins: [
     tailwindcss(),
     vue(),
